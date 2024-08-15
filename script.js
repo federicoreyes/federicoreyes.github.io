5ee867f72b14b476cd86e4edc6893e8d8888a667
federicoreyes <153644691+federicoreyes@users.noreply.github.com>
2024-08-15 13:51:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ const getCurrDate = function () {
 };
 
 const imagesToLoad = [
+  'img/trucombo-cassette-abierto.png',
+  'img/fotografia-cassettes-piano.png',
+  'img/arp-improv-no1.png',
   'img/fede-piano-barcelona.png',
   'img/fede-kew-gardens-cortada.png',
+  'img/7-10-22.png',
+  'img/tru-combo.png',
+  'img/fotografia-vol1.png',
+  'img/fotografia-vol2.png',
+  'img/hidden-toys.png',
+  'img/design-cuando-eramos-felices-fondo.png',
+  'img/design-fr.png',
+  'img/design-modern-better.png',
+  'img/design-musica-clasica-fondo.png',
+  'img/design-nanen.png',
+  'img/design-pasado-oscuro.png',
 ];
 
 function preloadImage(imageUrl) {
@@ -25,21 +39,3 @@ async function preloadImages() {
 }
 
 preloadImages();
-
-/*
-const anchors = document.querySelectorAll('a[href^="#"]');
-
-anchors.forEach(anchor => {
-  anchor.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    const target = document.getElementById(this.href.slice(1));
-    const headerHeight = document.querySelector('header').offsetHeight;
-
-    window.scrollTo({
-      top: target.offsetTop - headerHeight,
-      behavior: 'smooth',
-    });
-  });
-});
-*/
